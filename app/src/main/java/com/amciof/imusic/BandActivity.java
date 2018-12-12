b6ed33f41434eab8115b04e6aaea4b32a8372578
Vyacheslav Maharenok <amcishow@gmail.com>
2018-12-12 23:39:24 +0300

    Add database

---
+++ app/src/main/java/com/amciof/imusic/BandActivity.java
@@ -0,0 +1,136 @@
+package com.amciof.imusic;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class BandActivity extends AppCompatActivity {
+
+    private final static String TAG = "BandActivity";
+
+    EditText nameBox;
+    EditText infoBox;
+    Button delButton;
+    Button saveButton;
+
+    DBHelper dbHelper;
+    SQLiteDatabase db;
+    Cursor bandCursor;
+
+    long userId = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_band);
+
+        nameBox = (EditText) findViewById(R.id.editName);
+        infoBox = (EditText) findViewById(R.id.editInfo);
+        delButton = (Button) findViewById(R.id.deleteButton);
+        saveButton = (Button) findViewById(R.id.saveButton);
+
+        dbHelper = new DBHelper(this);
+        db = dbHelper.getWritableDatabase();
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            userId = extras.getLong("id");
+        }
+
+        if (userId > 0) {
+            bandCursor = db.rawQuery("select * from " + dbHelper.TABLE_BANDS + " where " +
+                    dbHelper.KEY_ID + "=?", new String[]{String.valueOf(userId)});
+            bandCursor.moveToFirst();
+            nameBox.setText(bandCursor.getString(1));
+            infoBox.setText(String.valueOf(bandCursor.getInt(2)));
+            bandCursor.close();
+        } else {
+            delButton.setVisibility(View.GONE);
+        }
+
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected  void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected  void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+
+    public void save(View view){
+        ContentValues cv = new ContentValues();
+        cv.put(dbHelper.KEY_NAME, nameBox.getText().toString());
+        cv.put(dbHelper.KEY_INFO, Integer.parseInt(infoBox.getText().toString()));
+
+        if (userId > 0) {
+            db.update(dbHelper.TABLE_BANDS, cv, dbHelper.KEY_ID + "=" + String.valueOf(userId), null);
+        } else {
+            db.insert(dbHelper.TABLE_BANDS, null, cv);
+        }
+        goHome();
+    }
+
+    public void delete(View view){
+        db.delete(dbHelper.TABLE_BANDS, "_id = ?", new String[]{String.valueOf(userId)});
+        goHome();
+    }
+
+
+    private void goHome(){
+        db.close();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+    }
+
+}
--- app/src/main/java/com/amciof/imusic/MainActivity.java
+++ app/src/main/java/com/amciof/imusic/MainActivity.java
@@ -1,31 +1,57 @@
 package com.amciof.imusic;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.Arrays;
+import static com.amciof.imusic.DBHelper.KEY_INFO;
+import static com.amciof.imusic.DBHelper.KEY_NAME;
+import static com.amciof.imusic.DBHelper.TABLE_BANDS;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = "MainActivity";
 
-    private String[] bands = { "Imagine Dragons", "Red Hot Chili Peppers", "Nirvana", "Queen", "Coldplay"};
+    TextView dbInfo;
+    ListView bandList;
+
+    DBHelper dbHelper;
+    SQLiteDatabase db;
+    Cursor bandCursor;
+    SimpleCursorAdapter bandAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView countriesList = (ListView) findViewById(R.id.bandsList);
-        Arrays.sort(bands);
-        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, bands);
-        countriesList.setAdapter(adapter);
+        String app_hello =  getResources().getString(R.string.app_hello);
+        Toast toast = Toast.makeText(this, app_hello, Toast.LENGTH_LONG);
+        toast.show();
+
+        dbInfo = (TextView) findViewById(R.id.dbInfo);
+        bandList = (ListView) findViewById(R.id.bandList);
+        bandList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), BandActivity.class);
+                intent.putExtra("id", id);
+                startActivity(intent);
+            }
+        });
+
+        dbHelper = new DBHelper(this);
 
         Log.d(TAG, "onCreate");
     }
@@ -33,6 +59,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy(){
         super.onDestroy();
+
+        db.close();
+        bandCursor.close();
+
         Log.d(TAG, "onDestroy");
     }
 
@@ -57,6 +87,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
+
+        db = dbHelper.getReadableDatabase();
+
+        bandCursor =  db.rawQuery("select * from "+ TABLE_BANDS, null);
+
+        String[] headers = new String[] {KEY_NAME, KEY_INFO};
+
+        bandAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item, bandCursor, headers, new int[] {android.R.id.text1, android.R.id.text2}, 0);
+        dbInfo.setText("Найдено элементов: " + String.valueOf(bandCursor.getCount()));
+        bandList.setAdapter(bandAdapter);
+
         Log.d(TAG, "onResume");
     }
 
@@ -99,6 +140,11 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void add(View view){
+        Intent intent = new Intent(this, BandActivity.class);
+        startActivity(intent);
     }
 
 }
